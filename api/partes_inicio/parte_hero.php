543a7a890537d225aed2dae3690006caf0f6e5a4
Mirtilo122 <160802640+Mirtilo122@users.noreply.github.com>
2024-11-29 16:49:19 -0300

    Update parte_hero.php

--- api/partes_inicio/parte_hero.php
+++ api/partes_inicio/parte_hero.php
@@ -1,7 +1,7 @@
 <div class="hero-section" style="background-image: url('imagens/fundo.png');" id="inicio">
   <div class="overlay"></div>
   <div class="content">
-    <h1>Administração e Desenvolvimento de Sistemas</h1>
+    <h1>Análise e Desenvolvimento de Sistemas</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
     <br>
     <button type="button" class="btn btn-success" onclick="location.href='#sobre'">Conheça o curso</button>
